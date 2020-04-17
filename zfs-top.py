3173af772ae64f22734b3272771f44d4cdd64988
Ramy Allam <linux@ramyallam.com>
2020-04-17 10:49:50 +0200

    Feature: Add support for Reads monitoring

--- zfs-top.py
+++ zfs-top.py
@@ -34,6 +34,26 @@ def get_dataset_nwrites(objset_id):
     return dataset_nwrites
 
 
+def get_dataset_reads(objset_id):
+    objset_file = "{}/{}".format(objset_dir, objset_id)
+
+    with open(objset_file, 'r') as f:
+        content = f.readlines()
+
+    dataset_reads = int(content[5].split()[2])
+    return dataset_reads
+
+
+def get_dataset_nread(objset_id):
+    objset_file = "{}/{}".format(objset_dir, objset_id)
+
+    with open(objset_file, 'r') as f:
+        content = f.readlines()
+
+    dataset_nread = float(content[6].split()[2])
+    return dataset_nread
+
+
 def main():
     # List of objset
     # Example: ['objset-0x1026', 'objset-0x1041', 'objset-0x10424']
@@ -59,12 +79,16 @@ def main():
             objset_results[objset_id]["dataset_name"] = get_dataset_name(objset_id)
             objset_results[objset_id]["writes_iops_{}".format(test_id)] = get_dataset_writes(objset_id)
             objset_results[objset_id]["writes_bandwidth_{}".format(test_id)] = get_dataset_nwrites(objset_id)
+            objset_results[objset_id]["reads_iops_{}".format(test_id)] = get_dataset_reads(objset_id)
+            objset_results[objset_id]["reads_bandwidth_{}".format(test_id)] = get_dataset_nread(objset_id)
 
         time.sleep(2)
 
-    # Set variables for cumulative total IOPS and bandwidth 
+    # Set variables for cumulative total IOPS and bandwidth
     writes_iops_p_second_total = 0
     writes_bandwidth_p_second_total = 0
+    reads_iops_p_second_total = 0
+    reads_bandwidth_p_second_total = 0
 
     # Loop through the modules results in objset_results dict and export the required information to final_results dict
     for objset_id in objset_results:
@@ -75,40 +99,81 @@ def main():
         writes_bandwidth_1 = objset_results[objset_id]["writes_bandwidth_1"]
         writes_iops_p_second = writes_iops_1 - writes_iops_0
         writes_bandwidth_p_second = round(float((writes_bandwidth_1 - writes_bandwidth_0) / 1024 / 1024), 3)
+        reads_iops_0 = objset_results[objset_id]["reads_iops_0"]
+        reads_iops_1 = objset_results[objset_id]["reads_iops_1"]
+        reads_bandwidth_0 = objset_results[objset_id]["reads_bandwidth_0"]
+        reads_bandwidth_1 = objset_results[objset_id]["reads_bandwidth_1"]
+        reads_iops_p_second = reads_iops_1 - reads_iops_0
+        reads_bandwidth_p_second = round(float((reads_bandwidth_1 - reads_bandwidth_0) / 1024 / 1024), 3)
 
         final_results[objset_id] = {}
         final_results[objset_id] = {
             "dataset_name": dataset_name,
             "writes_iops_p_second": writes_iops_p_second,
-            "writes_bandwidth_p_second": writes_bandwidth_p_second
+            "writes_bandwidth_p_second": writes_bandwidth_p_second,
+            "reads_iops_p_second": reads_iops_p_second,
+            "reads_bandwidth_p_second": reads_bandwidth_p_second
         }
 
         # Calculate cumulative total IOPS and bandwidth
         writes_iops_p_second_total += writes_iops_p_second
         writes_bandwidth_p_second_total += writes_bandwidth_p_second
+        reads_iops_p_second_total += reads_iops_p_second
+        reads_bandwidth_p_second_total += reads_bandwidth_p_second
 
-    # A little bit formating
+    # A little bit formatting
     writes_bandwidth_p_second_total = round(writes_bandwidth_p_second_total, 3)
+    reads_bandwidth_p_second_total = round(reads_bandwidth_p_second_total, 3)
 
     # Sort the keys in final_results dict to sort the output
     sorted_keys = sorted(objset_results, key=lambda x: (final_results[x]['writes_iops_p_second']), reverse=True)
 
     # Print the headers
-    print("\n--------------------------------------------------------------------------------------------")
-    print('{:<40s}{:<40s}{:<40s}'.format("", str("TotalWriteOps/s"), str("TotalWritesMB/s")))
-    print('{:<40s}{:<40s}{:<40s}\n'.format("", str(writes_iops_p_second_total), str(writes_bandwidth_p_second_total)))
-    print('{:<40s}{:<40s}{:<40s}'.format("DatasetName", str("WriteOps/s"), str("WritesMB/s")))
-    print("--------------------------------------------------------------------------------------------")
+    print("\n"
+          "-----------------------------------------------------------------------------------------------------------"
+          )
+    print('{:<33s}{:<20}{:<20s}{:<20s}{:<20s}'.format(
+            "",
+            str("TotalWriteOps/s"),
+            str("TotalWritesMB/s"),
+            str("TotalReadsOps/s"),
+            str("TotalReadsMB/s")
+            ))
+
+    print('{:<33s}{:<20s}{:<20s}{:<20s}{:<20s}\n'.format(
+            "",
+            str(writes_iops_p_second_total),
+            str(writes_bandwidth_p_second_total),
+            str(reads_iops_p_second_total),
+            str(reads_bandwidth_p_second_total)
+            ))
+
+    print('{:<33s}{:<20s}{:<20s}{:<20s}{:<20s}'.format(
+            "DatasetName",
+            str("WriteOps/s"),
+            str("WritesMB/s"),
+            str("ReadOps/s"),
+            str("ReadsMB/s")
+            ))
+
+    print("-----------------------------------------------------------------------------------------------------------")
 
     # Loop through the sorted keys and print the results to the user
     for objset_id in sorted_keys:
         dataset_name = final_results[objset_id]['dataset_name']
         writes_iops_p_second = final_results[objset_id]['writes_iops_p_second']
         writes_bandwidth_p_second = final_results[objset_id]['writes_bandwidth_p_second']
+        reads_iops_p_second = final_results[objset_id]['reads_iops_p_second']
+        reads_bandwidth_p_second = final_results[objset_id]['reads_bandwidth_p_second']
 
         if writes_iops_p_second > 0 or writes_bandwidth_p_second > 0:
-            print('{:<40s}{:<40s}{:<40s}'.format(
-                dataset_name, str(writes_iops_p_second), str(writes_bandwidth_p_second))
+            print('{:<33s}{:<20s}{:<20s}{:<20s}{:<20s}'.format(
+                dataset_name,
+                str(writes_iops_p_second),
+                str(writes_bandwidth_p_second),
+                str(reads_iops_p_second),
+                str(reads_bandwidth_p_second)
+                )
             )
 
 
